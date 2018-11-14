402fd56d18584f0d815c269f9cfbe153248a8636
Taiwo Akinosho <tiwiex@yahoo.com>
2018-11-14 05:59:42 +0100

    hero details generated

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,15 +10,17 @@ import { HEROES } from './mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  } 
 
 }
