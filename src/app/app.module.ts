3cb8b72e8881902d266dab143777d1cf3a471055
taiwo <tiwiex@yahoo.com>
2018-11-17 19:27:07 +0100

    added servers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
